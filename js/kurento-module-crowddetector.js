27b88780495da4275f6003bb700b2510c2aec368
jenkins <jenkins@kurento.org>
2016-09-12 11:12:21 +0000

    Generated code from kurento-module-crowddetector-js:29a3ba5

--- js/kurento-module-crowddetector.js
+++ js/kurento-module-crowddetector.js
@@ -653,7 +653,7 @@ exports.RelativePoint = RelativePoint;
  */
 
 Object.defineProperty(exports, 'name',    {value: 'crowddetector'});
-Object.defineProperty(exports, 'version', {value: '6.6.0'});
+Object.defineProperty(exports, 'version', {value: '6.6.1-dev'});
 
 
 var CrowdDetectorFilter = require('./CrowdDetectorFilter');
